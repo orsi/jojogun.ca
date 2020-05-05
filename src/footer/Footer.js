afb1ea63b67dce857d785de717f27d928df6a807
jorsi <jonathon.orsi@gmail.com>
2020-05-05 11:15:01 -0400

    add footer component

---
+++ src/footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './Footer.scss';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="Footer">
+        <small>&copy; For the people</small>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
