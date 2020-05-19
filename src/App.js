a5b5403e23884660caa6301a52d8efd4b482258d
jorsi <jonathon.orsi@gmail.com>
2020-05-19 13:30:24 -0400

    remove out of date references

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.scss';
 import Header from "./header/Header";
-import Footer from "./footer/Footer";
-import SpotifyContainer from "./spotify-container/SpotifyContainer";
-import ShowsContainer from "./shows-container/ShowsContainer";
-import VideosContainer from "./videos-container/VideosContainer";
 import Background from './background/Background';
 
 function App() {
